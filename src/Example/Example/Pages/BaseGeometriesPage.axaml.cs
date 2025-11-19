7c1c857637b553c7bcef1f0a6e1b1e000fdc1a64
CeSun <ce.sun@outlook.com>
2025-11-19 19:46:48 +0800

    #4 base geometries example page

--- src/Example/Example/Pages/BaseGeometriesPage.axaml.cs
+++ src/Example/Example/Pages/BaseGeometriesPage.axaml.cs
@@ -1,6 +1,17 @@
+using Aura3D.Avalonia;
+using Aura3D.Core;
+using Aura3D.Core.Geometries;
+using Aura3D.Core.Nodes;
+using Aura3D.Core.Resources;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+using System;
+using System.Drawing;
+using System.Numerics;
+using System.Threading.Tasks;
 
 namespace Example.Pages;
 
@@ -9,5 +20,129 @@ public partial class BaseGeometriesPage : UserControl
     public BaseGeometriesPage()
     {
         InitializeComponent();
+        box = new BoxGeometry();
+        sphere = new SphereGeometry();
+        cylinder = new CylinderGeometry();
+        plane = new PlaneGeometry();
+    }
+
+    BoxGeometry box;
+    SphereGeometry sphere;
+    CylinderGeometry cylinder;
+    PlaneGeometry plane;
+
+    private async void Aura3DView_SceneInitialized(object? sender, RoutedEventArgs e)
+    {
+        var view = sender as Aura3DView;
+
+        if (view == null)
+        {
+            return;
+        }
+        try
+        {
+            var camera = new Camera();
+
+            camera.ClearColor = Color.AliceBlue;
+
+            camera.ClearType = ClearType.Color;
+
+            camera.NearPlane = 0.1f;
+
+            camera.FarPlane = 50;
+
+            view.AddNode(camera);
+
+            var mesh = new Mesh();
+
+            mesh.Geometry = box;
+
+            mesh.Material = new Material();
+
+            Texture? texture = null;
+
+            texture = await Task.Run(() =>
+            {
+
+                using (var stream = AssetLoader.Open(new Uri($"avares://Example/Assets/Textures/background.jpg")))
+                {
+                    return TextureLoader.LoadTexture(stream);
+                }
+            });
+
+            mesh.Material.Channels = [
+                new () {
+                Name = "BaseColor",
+                Texture = texture,
+            }
+            ];
+
+            mesh.Material.BlendMode = BlendMode.Opaque;
+
+            mesh.Material.DoubleSided = true;
+
+            mesh.Position = camera.Position + camera.Forward * 3;
+
+            mesh.RotationDegrees = new Vector3(90, 0, 0);
+
+            view.AddNode(mesh);
+
+
+            var pl = new PointLight();
+
+            pl.LightColor = Color.White;
+
+            pl.CastShadow = true;
+
+            pl.AttenuationRadius = 2;
+
+            view.AddNode(pl);
+
+            this.mesh = mesh;
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    float pitch = 0;
+
+    Mesh? mesh;
+    Action<double>? update;
+    private void Aura3DView_SceneUpdated(object? sender, UpdateRoutedEventArgs e)
+    {
+        if (this.mesh == null)
+            return;
+        pitch += (float)(e.DeltaTime * 10);
+        mesh?.RotationDegrees = new Vector3(pitch, 0, 0);
+    }
+
+    private void Button_Click(object? sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        if (button == null)
+            return;
+        if (mesh == null)
+            return;
+        var s = button.Content?.ToString();
+        switch (s)
+        {
+            case "Box":
+                mesh.Geometry = box;
+                break;
+            case "Sphere":
+                mesh.Geometry = sphere;
+                break;
+            case "Cylinder":
+                mesh.Geometry = cylinder;
+                break;
+            case "Plane":
+                mesh.Geometry = plane;
+                break;
+            default: 
+                break;
+        }
     }
 }
