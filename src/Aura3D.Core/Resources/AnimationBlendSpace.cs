6567e4dd6ee3d1d5e5ca7284b8e27ccb781c5255
CeSun <ce.sun@outlook.com>
2026-02-05 22:57:13 +0800

    blendspace

--- src/Aura3D.Core/Resources/AnimationBlendSpace.cs
+++ src/Aura3D.Core/Resources/AnimationBlendSpace.cs
@@ -12,16 +12,20 @@ public class AnimationBlendSpace : IAnimationSampler
     {
         Skeleton = skeleton;
 
-        BonesTransform = new Matrix4x4[skeleton.Bones.Count];
+        bonesTransform = new Matrix4x4[skeleton.Bones.Count];
     }
 
     public Skeleton Skeleton { get; private set; }
 
-    public bool NeedUpdate { get; set; }
+    public bool NeedUpdate { get; set; } = true;
 
-    public IReadOnlyList<Matrix4x4> BonesTransform { get; private set; }
+    public IReadOnlyList<Matrix4x4> BonesTransform => bonesTransform;
 
-    Dictionary<Vector2, IAnimationSampler> AnimationSamplerMaps = [];
+    public Matrix4x4[] bonesTransform;
+
+    List <(Vector2, IAnimationSampler)> animationSamplers = [];
+
+    List<float> weights = new List<float>();
 
     public void AddAnimationSampler(Vector2 point, IAnimationSampler animationSampler)
     {
@@ -31,7 +35,8 @@ public class AnimationBlendSpace : IAnimationSampler
         if (point.Y > 1 || point.Y < -1)
             throw new Exception();
 
-        AnimationSamplerMaps.Add(point, animationSampler);
+        animationSamplers.Add((point, animationSampler));
+        weights.Add(0);
 
     }
     Vector2 AxisValue = default;
@@ -43,19 +48,51 @@ public class AnimationBlendSpace : IAnimationSampler
         AxisValue.X = x;
         AxisValue.Y = y;
     }
+    public float IdwPower { get; set; } = 2f;
 
     public void Update(double deltaTime)
     {
-        foreach(var (point, anim) in AnimationSamplerMaps)
+        float totalRawWeight = 0f;
+
+        int index = 0;
+        foreach (var (point, anim) in animationSamplers)
         {
             float distance = CalculateDistance(AxisValue.X, AxisValue.Y, point.X, point.Y);
             
             if (distance < 0.000001)
             {
-                
-            }
+                anim.Update(deltaTime);
 
+                for(int i = 0; i < BonesTransform.Count; i++)
+                {
+                    bonesTransform[i] = anim.BonesTransform[i];
+                }
+                return;
+            }
+            weights[index] = 1f / (float)MathF.Pow(distance, IdwPower);
+            totalRawWeight += weights[index];
+
+            index++;
         }
+
+        index = 0;
+        foreach (var result in weights)
+        {
+            float weight = result / totalRawWeight;
+            if (weight > 0.001)
+            {
+                animationSamplers[index].Item2.Update(deltaTime);
+                for (int i = 0; i < BonesTransform.Count; i++)
+                {
+                    if (index == 0)
+                        bonesTransform[i] =  animationSamplers[index].Item2.BonesTransform[i] * weight;
+
+                    bonesTransform[i] = bonesTransform[i] + animationSamplers[index].Item2.BonesTransform[i] * weight;
+                }
+            }
+            index++;
+        }
+
     }
 
     private float CalculateDistance(float x1, float y1, float x2, float y2)
