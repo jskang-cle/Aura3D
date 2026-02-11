692286e41a76dc50112af840fe35976f64ddbde1
CeSun <ce.sun@outlook.com>
2026-02-11 17:57:58 +0800

    pbr

--- src/Aura3D.Core/Assets/Shaders/deferredbase.fs
+++ src/Aura3D.Core/Assets/Shaders/deferredbase.fs
@@ -15,18 +15,25 @@ uniform sampler2d Texture_Occlusion;
 
 
 in vec2 vTexCoord;
+in vec3 vFragPosition;
+in mat3 vTBN;
+in vec3 vNormal;
 
 void main() 
 {
     vec4 baseColor = texture(Texture_BaseColor, vTexCoord);
-    vec4 normal = texture(Texture_Normal, vTexCoord);
+    vec3 normal = texture(Texture_Normal, vTexCoord).xyz;
     vec4 metalness = texture(Texture_Metalness, vTexCoord);
     vec4 roughness = texture(Texture_Roughness, vTexCoord);
     vec4 emissive = texture(Texture_Emissive, vTexCoord);
     vec4 occlusion = texture(Texture_Occlusion, vTexCoord);
 
+    
+    normal = normalize(normal.xyz * 2.0 - 1.0);
+    normal = normalize(vTBN * normal);
+
     Buffer_BaseColor_Metalness = vec4(baseColor.rgb, metalness.r);
-    Buffer_Normal_Roughness = vec4(normal.rgb, roughness.r);
+    Buffer_Normal_Roughness = vec4(normal, roughness.r);
     Buffer_Emissive_Occlusion = vec4(emissive.rgb, occlusion.r);
 
 }
